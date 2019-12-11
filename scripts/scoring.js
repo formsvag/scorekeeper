3e3b0c8b6c772a66b965eea4537255177b462ae8
ifs-rihose <rikard.hoijertz@ifsworld.com>
2019-12-11 16:30:11 +0100

    initial commit

--- scripts/scoring.js
+++ scripts/scoring.js
@@ -1,1 +1,27 @@
 
+function Add(id) {
+    var clicks = parseInt(document.getElementById(id).innerHTML);
+    clicks += 1;
+    document.getElementById(id).innerHTML = clicks;
+ };
+
+ function Sub(id) {
+    var clicks = parseInt(document.getElementById(id).innerHTML);
+    if (clicks > 0) {
+       clicks -= 1;
+    }
+    document.getElementById(id).innerHTML = clicks;
+ };
+
+/*
+  Function to save previous week/session to db
+ */
+function New() {
+    // Save data to db
+    Clear();
+}
+
+function Clear() {
+   document.getElementById('A').innerHTML = 0;
+   document.getElementById('B').innerHTML = 0;
+}
